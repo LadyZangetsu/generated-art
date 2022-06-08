eb4e9d02db344c2fe251b56358a8b5f8cd7da77c
LadyZangetsu <95544922+LadyZangetsu@users.noreply.github.com>
2022-06-08 20:10:47 +0200

    Bugfix: starting on an existing point

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ import random
 
 
 def step_once(t, a, b, existing_point):
-    print("right")
     [x, y] = t.pos()
-    if [x + a, y + b] in existing_point:
-        print("skip")
-    else:
+    if [x + a, y + b] not in existing_point:
         t.setx(x + a)
         t.sety(y + b)
         existing_point += [[t.xcor(), t.ycor()]]
-
     return existing_point
 
 
@@ -24,10 +20,16 @@ def get_adjacent_point(step_size):
     return adj_points
 
 
+def random_shift(step_size, shift):
+    a, b = random.randint(-shift, shift), random.randint(-shift, shift)
+    return a * step_size, b * step_size
+
+
 if __name__ == '__main__':
     t = turtle.Turtle()
 
     step_size = 10
+    shift = step_size
     existing_point = [[t.xcor(), t.ycor()]]
     while len(existing_point) < 10000:
         i = random.choice(range(4))
@@ -43,9 +45,17 @@ if __name__ == '__main__':
 
         if all([point in existing_point for point in get_adjacent_point(step_size)]):
             t.penup()
-            [x, y] = t.pos()
-            a, b = random.randint(-10, 10), random.randint(-10, 10)
-            t.goto(a*step_size, b*step_size)
+
+            x_new, y_new = random_shift(step_size, shift)
+            points_checked = 0
+            while [x_new, y_new] in existing_point:
+                x_new, y_new = random_shift(step_size, shift)
+                points_checked += 1
+                if points_checked > shift * 10:
+                    print("shift")
+                    shift += int(step_size/2)
+
+            t.goto(x_new, y_new)
             t.pendown()
             existing_point += [[t.xcor(), t.ycor()]]
         else:
